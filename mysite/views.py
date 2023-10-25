43f67cfc0a946180844fe19cb68585cb285ee5c3
Piotr Karmelita <piotr@karmelita.eu>
2023-10-25 19:51:28 +0200

    Add ffine tag system to templates

--- mysite/views.py
+++ mysite/views.py
@@ -5,6 +5,7 @@ from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
+from taggit.models import Tag
 
 
 class PostListView(ListView):
@@ -15,8 +16,12 @@ class PostListView(ListView):
     template_name = 'mysite/post/list.html'
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
     try:
@@ -28,7 +33,7 @@ def post_list(request):
 
     return render(request,
                   'mysite/post/list.html',
-                  {'posts': posts})
+                  {'posts': posts, 'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
