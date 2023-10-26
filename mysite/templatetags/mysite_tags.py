08878dcdc8a4d789f6a49532a484b37a8c0ce3d0
Piotr Karmelita <piotr@karmelita.eu>
2023-10-26 19:11:13 +0200

    add maarkdown

--- mysite/templatetags/mysite_tags.py
+++ mysite/templatetags/mysite_tags.py
@@ -1,4 +1,7 @@
 from django import template
+from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
 from ..models import Post
 
 
@@ -12,3 +15,15 @@ def total_posts():
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts': latest_posts}
+
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(
+        total_comments=Count('comments')
+    ).order_by('-total_comments')[:count]
+
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
