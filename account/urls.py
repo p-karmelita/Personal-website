78e2a94118e27621f7f87e947c506590a26e113f
Piotr Karmelita <piotr@karmelita.eu>
2023-11-01 23:00:50 +0100

    Change password view

--- account/urls.py
+++ account/urls.py
@@ -2,9 +2,17 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 
+
 urlpatterns = [
     # path('login/', views.user_login, name='login'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # adresy URL przeznaczone do obsługi zmiany hasła
+    path('password-change/',
+         auth_views.PasswordChangeView.as_view(),
+         name='password_change'),
+    path('password-change/done/',
+         auth_views.PasswordChangeDoneView.as_view(),
+         name='password_change_done'),
     path('dashboard/', views.dashboard, name='dashboard'),
 ]
