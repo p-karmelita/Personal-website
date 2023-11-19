c7383bb0fdfb884d2a3f960687190911e8bc71af
Piotr Karmelita <piotr@karmelita.eu>
2023-11-19 16:56:59 +0100

    Add account edit form and view

--- account/urls.py
+++ account/urls.py
@@ -30,4 +30,5 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
 ]
