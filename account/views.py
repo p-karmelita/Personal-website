9d9aba73b2b5fd4570d352db5ab6f5ecb31c0bbf
Piotr Karmelita <piotr@karmelita.eu>
2023-11-19 18:06:54 +0100

    use django messages framework

--- account/views.py
+++ account/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile
 
@@ -67,6 +68,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Uaktualnianie profilu zakończyło się sukcesem')
+        else:
+            messages.error(request, 'Wystąpił błąd podczas uaktualniania profilu.')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
