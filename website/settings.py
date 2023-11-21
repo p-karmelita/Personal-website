0c0e9efc44727731035b6998961c3436348100fa
Piotr Karmelita <piotr@karmelita.eu>
2023-11-21 22:35:41 +0100

    fix

--- website/settings.py
+++ website/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['moja-witryna.pl', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['karmelita.eu', 'localhost', '127.0.0.1']
 
 SITE_ID = 1
 
