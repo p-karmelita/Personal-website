0339d4202b007c1979e3e803b14a7eae73366774
Piotr Karmelita <piotr@karmelita.eu>
2023-12-15 21:57:27 +0100

    fix edit profile and add nice and crispy field from django crispy fields

--- account/views.py
+++ account/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
@@ -62,14 +62,15 @@ def dashboard(request):
 
 @login_required
 def edit(request):
+    try:
+        profile = request.user.profile
+    except Profile.DoesNotExist:
+        profile = Profile.objects.create(user=request.user)
+
     if request.method == 'POST':
-        user_form = UserEditForm(instance=request.user,
-                                 data=request.POST)
-        profile_form = ProfileEditForm(
-            instance=request.user.profile,
-            data=request.POST,
-            files=request.FILES
-        )
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=profile, data=request.POST, files=request.FILES)
+
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
@@ -78,7 +79,6 @@ def edit(request):
             messages.error(request, 'Wystąpił błąd podczas uaktualniania profilu.')
     else:
         user_form = UserEditForm(instance=request.user)
-        profile_form = ProfileEditForm(instance=request.user.profile)
-    return render(request,'account/edit.html',
-                  {'user_form': user_form,
-                   'profile_form': profile_form})
+        profile_form = ProfileEditForm(instance=profile)
+
+    return render(request, 'account/edit.html', {'user_form': user_form, 'profile_form': profile_form})
